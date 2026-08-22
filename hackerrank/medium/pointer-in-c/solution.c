f9e79c43c6d7c61b4e876f5308307de7d9a2ca92
Nakul Nandwana <103959885+nakul2006@users.noreply.github.com>
2026-08-22 16:03:31 +0530

    ✅ Pointers in C | Medium | C | Runtime: N/A | Memory: N/A | 2026-08-22

--- hackerrank/medium/pointer-in-c/solution.c
+++ hackerrank/medium/pointer-in-c/solution.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-int max_of_four(int a, int b, int c, int d)
-{
-    int max = a;
-    if(b > max)
-        max = b;
-    if(c > max)
-        max = c;
-    if(d > max)
-        max = d;
-    return max;
+
+void update(int *a,int *b) {
+    int sum = *a + *b;
+    int diff = abs(*a - *b);
+    *a = sum;
+    *b = diff;
 }
 
 int main() {
-    int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    int ans = max_of_four(a, b, c, d);
-    printf("%d", ans);
+    int a, b;
+    int *pa = &a, *pb = &b;
     
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
     return 0;
 }
